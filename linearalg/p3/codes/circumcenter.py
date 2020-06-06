e3b6956adf1d8dbd6cb04a147271f16118f43c12
chakki1234 <chakkirala.a@gmail.com>
2020-06-06 22:51:42 +0530

    added third problem

---
+++ linearalg/p3/codes/circumcenter.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from coeffs import *
+
+#if using termux
+import subprocess
+import shlex
+
+
+#Quad vertices
+A = np.array([6, -6])
+B = np.array([3, -7])
+C = np.array([3, 3])
+O = circumCenter(A, B, C)
+print(A, B, C, O)
+
+#generating lines
+x_AB = line_gen(A, B)
+x_BC = line_gen(B, C)
+x_CA = line_gen(C, A)
+
+#plot circle
+circle1 = plt.Circle((O[0], O[1]), np.linalg.norm(A-O), color='b', fill=False, linestyle='dotted')
+
+fig, ax = plt.subplots()
+ax.add_artist(circle1)
+ax.set_aspect('equal', adjustable='datalim')
+
+#plot points and lines
+ax.plot(x_AB[0,:], x_AB[1,:], linestyle='dotted', label='$AB$')
+ax.plot(x_BC[0,:], x_BC[1,:], linestyle='dotted', label='$BC$')
+ax.plot(x_CA[0,:], x_CA[1,:], linestyle='dotted', label='$CA$')
+ax.text(A[0], A[1] * (1 - 0.1), 'A')
+ax.plot(A[0], A[1] , 'o')
+ax.text(B[0], B[1] * (1 - 0.1), 'B')
+ax.plot(B[0], B[1], 'o')
+ax.text(C[0], C[1] * (1 + 0.1), 'C')
+ax.plot(C[0], C[1], 'o')
+ax.text(O[0], O[1] * (1 - 0.2), 'O')
+ax.plot(O[0], O[1], 'o')
+
+ax.legend(loc='best')
+
+# if using termux
+# fig('./figs/circle.eps')
+# fig('./figs/circle.pdf')
+# subprocess.run(shlex.split("termux-open ./figs/cyclic_quad.pdf"))
+# else
+# fig.savefig('./figs/circles.eps')

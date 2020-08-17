d61796672ce18f6bb77b11eae163fa783abb170c
chakki1234 <chakkirala.a@gmail.com>
2020-08-17 13:11:51 +0530

    minor changes in prob13

--- olympiad/prob_13/prob_13.c
+++ olympiad/prob_13/prob_13.c
@@ -23,9 +23,7 @@ int main()
 {
     int multiple_of = 15, no_of_elem_in_set = 1000, temp[no_of_elem_in_set], len = 0;
     
-    if(no_of_elem_in_set > multiple_of)
-    {
-        for(int i = 1; i < multiple_of; ++i)
+    for(int i = 1; i < multiple_of; ++i)
         {
             if(find(temp, i, len) == 0)
             {
@@ -34,7 +32,7 @@ int main()
                 len+=2;
             } 
         }
-        printf("The number of unnique elements is %d", len / 2 + 1);
-    } 
+        printf("The number of unique elements is %d", len / 2 + 1);
+     
     return 0;  
 }
